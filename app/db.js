7ed34bf0e50febc6ba13df0dfca1e97c17e52785
andianwar <>
2022-02-21 12:10:31 +0700

    url uri database

--- app/db.js
+++ app/db.js
@@ -1,16 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  // isi dengan password, username, dan detail database kalian sendiri
-  host: 'localhost',
-  user: 'lazuandi',
-  password: '',
-  database: 'synrgy',
-  // ignore below configuration for now, 
-  // this are pooling connection settings
-  max: 20, // connection pool ignore for now
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = {
